02f759f8f87cd0dfbd78a4c16e297e1cd52b8aa0
Chelsea Chen <core.editor>
2024-03-02 22:52:39 -0700

    add functions

--- Sync/app/src/main/java/com/example/sync/Administrator.java
+++ Sync/app/src/main/java/com/example/sync/Administrator.java
@@ -1,4 +1,13 @@
 package com.example.sync;
 
+import java.util.ArrayList;
+
 public class Administrator extends User{
+    private Profile browseProfile(ArrayList<Integer> profileNumber){
+        Profile profile = Profile.find(profileNumber);
+    }
+    private void editProfile(Profile editProfile){
+        editProfile.set();
+        editProfile.delete();
+    }
 }
--- Sync/app/src/main/java/com/example/sync/Attendee.java
+++ Sync/app/src/main/java/com/example/sync/Attendee.java
@@ -1,4 +1,18 @@
 package com.example.sync;
 
 public class Attendee extends User{
+    public void scanQRCode(){
+        new QRCodeScanActivity();
+    }
+    public void editProfile(){
+        Profile profile = new Profile();
+    }
+    public void pushNotification(){
+        ;
+    }
+    public void browseEvent(){
+        Event.get();
+        Event.display();
+    }
+
 }
