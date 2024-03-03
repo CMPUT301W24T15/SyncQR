71455663ebc66503440e060443659e585ecc892d
Chelsea Chen <core.editor>
2024-03-02 18:52:25 -0700

    edit user class

--- Sync/app/src/main/java/com/example/sync/User.java
+++ Sync/app/src/main/java/com/example/sync/User.java
@@ -1,4 +1,13 @@
 package com.example.sync;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class User {
+    String username;
+    ArrayList<Integer> password;
+
+    public User() {
+        this.username = "Visitor";
+    }
 }
