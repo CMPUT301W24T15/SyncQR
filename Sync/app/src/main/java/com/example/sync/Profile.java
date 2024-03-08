41c061ad126a56e2382b8e2f3c54ea9d1082641e
Henry Zhu <henryzhu@Henrys-MBP.lan>
2024-03-07 21:06:08 -0700

    1

--- Sync/app/src/main/java/com/example/sync/Profile.java
+++ Sync/app/src/main/java/com/example/sync/Profile.java
@@ -100,4 +100,36 @@ public class Profile extends AppCompatActivity {
         userHomepageInput.setText("");
         userContactInput.setText("");
     }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfilePictureUrl() {
+        return profilePictureUrl;
+    }
+
+    public void setProfilePictureUrl(String profilePictureUrl) {
+        this.profilePictureUrl = profilePictureUrl;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
+
