8bd6a0a1b0ff7cc306958063918bdeadbd9a7515
Chelsea Chen <core.editor>
2024-03-08 10:23:36 -0700

    wrongly deleted database

---
+++ Sync/app/src/main/java/com/example/sync/Database.java
@@ -0,0 +1,21 @@
+package com.example.sync;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
+public class Database {
+
+    public static void addEntry(String collectionName, String key, HashMap<String, String> value) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference currentRef = db.collection(collectionName);
+        currentRef.document(key).set(value);
+    }
+
+    public static void changeEntry(String collectionName, String key, HashMap<String, String> value) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference currentRef = db.collection(collectionName);
+        currentRef.document(key).set(value);
+    }
+}
