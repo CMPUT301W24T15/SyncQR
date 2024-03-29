2bc33b89b70b06d078a4747f68f12fc7e5fbb6c3
miaocnca@gmail.com <miaocnca@gmail.com>
2024-03-28 22:59:11 -0600

    Choose attendee and continue as guest can bring u back to home page as before.

--- Sync/app/src/main/java/com/example/sync/AttendeeLogin.java
+++ Sync/app/src/main/java/com/example/sync/AttendeeLogin.java
@@ -2,6 +2,7 @@ package com.example.sync;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -38,6 +39,15 @@ public class AttendeeLogin extends AppCompatActivity {
             String username = usernameEditText.getText().toString();
             String password = passwordEditText.getText().toString();
             login(username, password);
+        });
+
+        guestLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AttendeeLogin.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
         });
     }
 
