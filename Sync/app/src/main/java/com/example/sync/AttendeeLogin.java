8a10943e62e05f85e2bd250d5bf777558194a3af
“Hiren <hiren1@ualberta.ca>
2024-03-30 14:09:00 -0600

    Changes made to AttendeeLogin, now added guestLogin and resolved issue with passing userID

--- Sync/app/src/main/java/com/example/sync/AttendeeLogin.java
+++ Sync/app/src/main/java/com/example/sync/AttendeeLogin.java
@@ -9,7 +9,6 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -18,7 +17,6 @@ public class AttendeeLogin extends AppCompatActivity {
     private EditText usernameEditText;
     private EditText passwordEditText;
     private Button loginButton;
-
     private Button guestLoginButton;
     private Button helpButton;
 
@@ -31,27 +29,25 @@ public class AttendeeLogin extends AppCompatActivity {
         passwordEditText = findViewById(R.id.passwordEditText);
         loginButton = findViewById(R.id.loginButton);
         helpButton = findViewById(R.id.helpButtonLogin);
-
-        // Make sure this ID exists in your layout
         guestLoginButton = findViewById(R.id.guestButton);
 
         loginButton.setOnClickListener(view -> {
+
+            // Generate userID for account and pass to login() which will then pass to main activity
             String username = usernameEditText.getText().toString();
             String password = passwordEditText.getText().toString();
-            login(username, password);
+            String userId = UserIDGenerator.generateUserID();
+            login(username, password, userId);
         });
 
-        guestLoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(AttendeeLogin.this, MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-                startActivity(intent);
-            }
+        guestLoginButton.setOnClickListener(v -> {
+            // for guest login, generate userID and pass to continueAsGuest
+            String guestUserId = UserIDGenerator.generateUserID();
+            continueAsGuest(guestUserId);
         });
     }
 
-    private void login(String username, String password) {
+    private void login(String username, String password, String userId) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         db.collection("users")
@@ -59,27 +55,27 @@ public class AttendeeLogin extends AppCompatActivity {
                 .whereEqualTo("password", password)
                 .get()
                 .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        QuerySnapshot querySnapshot = task.getResult();
-                        if (!querySnapshot.isEmpty()) {
-                            // Redirect to AttendeeDashboard Activity
-                            Intent intent = new Intent(AttendeeLogin.this, AttendeeDashboard.class);
-                            startActivity(intent);
+                    if (task.isSuccessful() && task.getResult() != null && !task.getResult().isEmpty()) {
 
-                            // Toast that login is successful
-                            Toast.makeText(AttendeeLogin.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                        } else {
-
-                            // Toast that password was incorrect
-                            Toast.makeText(AttendeeLogin.this, "Login Failed: Incorrect username or password.", Toast.LENGTH_SHORT).show();
-                        }
-
+                        // If login successful, pass the userId to MainActivity
+                        Intent intent = new Intent(AttendeeLogin.this, MainActivity.class);
+                        intent.putExtra("userId", userId);
+                        startActivity(intent);
+                        Toast.makeText(AttendeeLogin.this, "Login Successful", Toast.LENGTH_SHORT).show();
                     } else {
-
-                        // Toast that process failed due to other issues (database connection)
-                        Toast.makeText(AttendeeLogin.this, "Login Failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-
+                        // Login failed due to user and password not being present
+                        Toast.makeText(AttendeeLogin.this, "Login Failed: Incorrect username or password.", Toast.LENGTH_SHORT).show();
                     }
-                });
+                    // Login failed due to other issues like network or accessing database
+                }).addOnFailureListener(e -> Toast.makeText(AttendeeLogin.this, "Login Failed: " + e.getMessage(), Toast.LENGTH_SHORT).show());
+    }
+
+    private void continueAsGuest(String guestUserId) {
+        // Create an intent for the MainActivity
+        Intent intent = new Intent(AttendeeLogin.this, MainActivity.class);
+
+        // Put the generated guest user ID as an extra in the intent and start activity
+        intent.putExtra("userId", guestUserId);
+        startActivity(intent);
     }
 }
